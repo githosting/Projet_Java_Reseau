577d0ff6a0f8e1ba3dcf9745be36ec5918a75536
githosting <alexandrebettinger@hotmail.fr>
2017-02-19 12:00:43 +0100

    Setting Snake GridPane and interactions.

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package morpionet;
 
 import java.net.URL;
@@ -11,35 +7,34 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Rectangle;
 
-/**
- *
- * @author Bettinger
- */
+
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label1;
-    private Rectangle r4;
-    private AnchorPane case4;
+    private Label label_info;
+
+    @FXML
+    private GridPane gp;
     
     // FXML Getters
-    public Label getLabel() { return label1; }
-    public Rectangle getRectangle() { return r4; }
-    public AnchorPane getCase() { return case4; }
+    public Label getLabelInfo() { return label_info; }
+    public GridPane getGridPane() { return gp; }
+    
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label1.setText("Hello World!");
+      
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
         // LE CODE ICI S'EXECUTE AVANT LE SHOW DE L'INTERFACE GRAPHIQUE.
-        r4.setFill(Color.RED);
-        
+ 
     }   
     
 }
--- MorpioNet.java
+++ MorpioNet.java
@@ -1,52 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package morpionet;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Bettinger
- */
+
 public class MorpioNet extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        
-        //FXMLLoader loader = new FXMLLoader();
-        //loader.setLocation(getClass().getResource("FXMLDocument.fxml"));
-        //Parent root = loader.load();
-        // Récupération du controller pour pouvoir ensuite modifier des éléments de la vue.
-        
-        
+
         FXMLLoader loader = new FXMLLoader();
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        loader.setLocation(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = loader.load();
         FXMLDocumentController controller = loader.getController();
         
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
         
-        // EXEMPLE POUR MODIFIER LA VUE
-        controller.getLabel().setText("KIKOO !");
         
-        controller.getLabel().setStyle("-fx-background-color: red;");
-        
-        //controller.getCase().setStyle("-fx-background-color: cornsilk;");
-        //controller.getCase().setStyle("-fx-background-color: red;");
-        //controller.getCase().setStyle("-fx-background-color: #ff3333");
-        
-        //javafx.scene.paint.Color color = javafx.scene.paint.Color.rgb(220, 20, 60);
-        //controller.getRectangle().setFill(color); 
-        
-        //rectangle.setFill(color);
+        controller.getLabelInfo().setText("KIKOO !");
+         
+        for (Node child : controller.getGridPane().getChildren()) 
+        {
+            child.setStyle("-fx-background-color: black;");
+            /*
+            if(child.getStyle() == "-fx-background-color: red;")
+                child.setStyle("-fx-background-color: black;");
+            */
+        }
+
     }
 
     /**
