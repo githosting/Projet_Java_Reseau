0cca86d793670edb2e88496004e867121877d4b5
githosting <alexandrebettinger@hotmail.fr>
2017-02-23 20:52:28 +0100

    Managing server and client sockets.

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -1,8 +1,15 @@
 
 package puissance4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 import javafx.event.*;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -14,6 +21,14 @@ import javafx.scene.layout.GridPane;
 
 public class FXMLDocumentController implements Initializable {
     
+    public static Socket socket = null;
+    private PrintWriter out = null;
+    private BufferedReader in = null;
+    private Scanner sc = null;
+    public static Thread thread_emission;
+    public static Thread thread_reception;
+        
+        
     @FXML
     private Label label_info;
 
@@ -38,8 +53,30 @@ public class FXMLDocumentController implements Initializable {
     }
     
     @FXML
-    private void handleButtonAction(ActionEvent event) 
+    private void connectClick(ActionEvent event) 
     {
+        System.out.println("CLICK");
+        
+        try 
+        {
+            System.out.println("Demande de connexion");
+            socket = new Socket("127.0.0.1",2013); // Sur le port 2013
+
+            // Si ce message s'affiche c'est que je suis connecté
+            System.out.println("Connexion établie avec le serveur."); 
+
+            out = new PrintWriter(socket.getOutputStream());
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));	
+            sc = new Scanner(System.in);
+
+            thread_emission = new Thread(new RUN_Emission(out));
+            thread_emission.start();
+            thread_reception = new Thread(new RUN_Reception(in));
+            thread_reception.start();
+        } 
+        catch (UnknownHostException e) { System.err.println("Impossible de se connecter à l'adresse "+socket.getLocalAddress()); } 
+        catch (IOException e) { System.err.println("Aucun serveur à l'écoute du port "+socket.getLocalPort()); }
+        
         /*
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         out = new PrintWriter(socket.getOutputStream());
